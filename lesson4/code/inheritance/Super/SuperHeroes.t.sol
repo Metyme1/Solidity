9d4fa059d5e8b0fbb4e1679930f573ca37f592e1
mitike <128352170+mitikmol@users.noreply.github.com>
2024-05-23 05:40:08 -0700

    Update SuperHeroes.t.sol

--- lesson4/code/inheritance/Super/SuperHeroes.t.sol
+++ lesson4/code/inheritance/Super/SuperHeroes.t.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/Hero.sol";
+import "../src/SuperHeroes.sol";
+import "../src/Enemy.sol";
+
+contract EscrowTest is Test {
+    Warrior public warrior;
+    Mage public mage;
+    Enemy public enemy;
+    
+    function setUp() public {
+        warrior = new Warrior();
+        mage = new Mage();
+        enemy = new Enemy();
+    }
+
+    function testWarriorAttack() public {
+        warrior.attack(enemy);
+        assertEq(enemy.health(), 50);
+        assertEq(warrior.energy(), 9);
+    }
+
+    function testMageAttack() public {
+        mage.attack(enemy);
+        assertEq(enemy.health(), 20);
+        assertEq(mage.energy(), 9);
+    }
+}
 
