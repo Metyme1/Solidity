bcf82bfce8fde5075b818fd2a2ce599cf5833937
mitike <128352170+mitikmol@users.noreply.github.com>
2024-05-23 05:28:45 -0700

    Update hero.sol

--- lesson4/code/inheritance/Constructor Args/hero.sol
+++ lesson4/code/inheritance/Constructor Args/hero.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Hero {
+	uint public health;
+	constructor(uint _health) {
+		health = _health;
+	}
+
+	function takeDamage(uint damage) public {
+		health -= damage;
+	}
+}
 
