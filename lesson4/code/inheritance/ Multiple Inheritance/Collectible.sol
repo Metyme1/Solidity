bc6b06b7334f4475ea376e3dacf3887302cd2f2f
mitike <128352170+mitikmol@users.noreply.github.com>
2024-05-23 05:48:16 -0700

    Update Collectible.sol

--- lesson4/code/inheritance/ Multiple Inheritance/Collectible.sol
+++ lesson4/code/inheritance/ Multiple Inheritance/Collectible.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import "./BaseContracts.sol";
+
+contract Collectible is Ownable, Transferable {
+	uint public price;
+
+	function markPrice(uint _price) external onlyOwner {
+		price = _price;
+	}
+}
