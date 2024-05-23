9a6fce41b04a68c29c3ae49b02b3358aab6cc546
mitike <128352170+mitikmol@users.noreply.github.com>
2024-05-23 05:32:41 -0700

    Update hero.sol

--- lesson4/code/inheritance/Virtual Override/hero.sol
+++ lesson4/code/inheritance/Virtual Override/hero.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import "./Enemy.sol";
+
+abstract contract Hero {
+	uint public health;
+	constructor(uint _health) {
+		health = _health;
+	}
+	
+	function takeDamage(uint damage) public {
+		health -= damage;
+	}
+	
+	enum AttackTypes { Brawl, Spell }
+	function attack(Enemy enemy) public virtual;
+}
